ad940d3e169c65b0b814fc5d7caa735612d6a8d4
刘洋 <117634430@qq.com>
2017-04-28 16:04:11 +0800

    文件夹及内置资源

---
+++ Assets/Editor/Chapter01/ExampleWindow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+using System.Reflection;
+
+public class ExampleWindow : EditorWindow
+{
+    // 运行时候调用editor脚本    [InitializeOnLoadMethod]
+
+    static void Initialize()
+    {
+        ExampleWindow window = (ExampleWindow)EditorWindow.GetWindow(typeof(ExampleWindow), true, "ExampleWindow");
+    }
+    [InitializeOnLoadMethod]
+    static void GetBultinAssetNames()
+    {
+        // 读取default中的文件
+        var tex = EditorGUIUtility.Load("logo.jpg") as Texture;
+        Debug.Log(tex);
+        // 加载内置资源
+        //var flags = BindingFlags.Static | BindingFlags.NonPublic;
+        //var info = typeof(EditorGUIUtility).GetMethod("GetEditorAssetBundle", flags);
+        //var bundle = info.Invoke(null, new object[0]) as AssetBundle;
+
+        //foreach (var n in bundle.GetAllAssetNames())
+        //{
+        //    Debug.Log(n);
+        //}
+    }
+}
---
+++ Assets/Editor/Chapter01/TestEditroFolder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+
+public class TestEditroFolder : MonoBehaviour {
+    void OnEnable()
+    {
+#if UNITY_EDITOR
+        EditorWindow.GetWindow<ExampleWindow>();
+#endif
+    }
+
+}
