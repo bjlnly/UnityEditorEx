02b678f47734ec8b5a9e4055bfdc45efa70458b1
刘洋 <117634430@qq.com>
2017-09-14 20:45:03 +0800

    一些propertydrawer的案例

---
+++ Assets/Editor/Chapter10/RangeDrawer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomPropertyDrawer(typeof(Range2Attribute))]
+public class RangeDrawer : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        Range2Attribute range2 = (Range2Attribute)attribute;
+
+        if (property.propertyType == SerializedPropertyType.Integer)
+        {
+            EditorGUI.IntSlider(position, property, range2.min, range2.max, label);
+        }
+        else
+        {
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
---
+++ Assets/Script/Chapter10/Hoge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Hoge : MonoBehaviour
+{
+    [SerializeField, Range2(0, 10)]
+    private int hp;
+    [SerializeField, Range2(0, 10)]
+    int hpRange;
+    [SerializeField, Range2(0, 10)]
+    float str;
+}
