86d06f34e8c4ae17eb9251538d400f4b53c2fb60
刘洋 <117634430@qq.com>
2017-09-27 21:10:14 +0800

    通过已有的资源创建资源

---
+++ Assets/Editor/Chapter11/DoCreateMaterialAsset.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+
+public class DoCreateMaterialAsset : EndNameEditAction
+{
+    public override void Action(int instanceId, string pathName, string resourceFile)
+    {
+        var mat = (Material)EditorUtility.InstanceIDToObject(instanceId);
+
+        mat.color = Color.red;
+
+        AssetDatabase.CreateAsset(mat, pathName);
+        AssetDatabase.ImportAsset(pathName);
+        ProjectWindowUtil.ShowCreatedAsset(mat);
+    }
+
+    [MenuItem("Assets/Create ExampleMaterailAssets")]
+    static void CreateExampleMaterialAssets()
+    {
+        var material = new Material(Shader.Find("Standard"));
+        var instanceID = material.GetInstanceID();
+
+        var icon = AssetPreview.GetMiniThumbnail(material);
+
+        var endNameEditAction = ScriptableObject.CreateInstance<DoCreateMaterialAsset>();
+
+        ProjectWindowUtil.StartNameEditingIfProjectWindowExists(instanceID, endNameEditAction, "New Material.mat",icon, "");
+    }
+}
